667bfaf8b1acbd19ec87d2e1aba91c028ef54f36
atxpaul <pablo.pan.veira@gmail.com>
2021-12-16 12:37:42 +0100

    Launch scrap job if api result is empty

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 
+const url = 'https://ganouodepor.herokuapp.com/';
+//const url = 'http://localhost:8080/';
 (async () => {
   setInterval(check, 300000);
 })();
 
-function check() {
-  let test = axios.get('https://ganouodepor.herokuapp.com/');
+async function check() {
+  let results = await axios.get(`${url}api/depor`);
+  if (Object.entries(results.data[0]).length === 0) {
+    let test = axios.get(`${url}api/depor/scrap`);
+  }
+  let test = axios.get(url);
 }
